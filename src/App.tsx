942897a565ad3503808dfe2fc8ad5c4edd9ff802
Max Faria <127763619+max-faria@users.noreply.github.com>
2023-12-08 19:38:57 +0530

    feat: grafico and avaliacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import './App.css';
+import Avaliacao from './components/Avaliacao';
+import Botao from './components/Botao';
 import Cabecalho from './components/Cabecalho';
 import Container from './components/Container';
+import Grafico from './components/Grafico';
 import Rodape from './components/Rodape';
+import Subtitulo from './components/Subtitulo';
 import Tabela from './components/Tabela';
 import Titulo from './components/Titulo';
 import useDadosConsulta from './useDadosConsulta';
+import useDadosProfisional from './useDadosProfissional';
 
 function App() {
-  const {dados, erro} = useDadosConsulta()
+  const {dados: consultas, erro: consultasErro} = useDadosConsulta()
+  const {dados: profissionais, erro: profissionalErro} = useDadosProfisional()
 
   return (
     <>
       <Cabecalho/>
       <Container>
         <Titulo>Área Administrativa</Titulo>
-      <Tabela consultas={dados}/>
+        <Botao>Cadastrar Especialista</Botao>
+        <Titulo imagem="consulta">Consultas do Dia</Titulo>
+        <Tabela consultas={consultas}/>
+        <Titulo imagem="grafico">Consultas mensais por especialista</Titulo>
+        <Subtitulo>Dezembro/22</Subtitulo>
+        <Grafico  profissionais={profissionais} consultas={consultas}/>
+        <Titulo imagem="avaliacao">Avaliações de especialistas</Titulo>
+        <Avaliacao profissionais={profissionais}/>
       </Container>
       <Rodape/>
     </> 
--- src/components/Grafico/index.tsx
+++ src/components/Grafico/index.tsx
@@ -1,12 +1,8 @@
-import { ResponsiveContainer } from "recharts";
-import { Bar } from "recharts";
-import { XAxis } from "recharts";
-import { YAxis } from "recharts";
-import { BarChart } from "recharts";
-import styled from "styled-components";
-import IConsulta from "../../types/IConsulta";
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import IProfissional from "../../types/IProfissional";
+import IConsulta from "../../types/IConsulta";
 import useDadosGrafico from "./useDadosGrafico";
+import styled from "styled-components";
 
 interface Props {
     profissionais: IProfissional[] | null,
@@ -18,28 +14,29 @@ interface IDados {
     consultas: number
 }
 
-const SecaoEstilizada = styled.section`
+const SecaoEtilizada = styled.section`
 background-color: var(--branco);
 border-radius: 16px;
 `
 
-function Grafico({ profissionais, consultas }: Props) {
-    let dados: Array<IDados> = useDadosGrafico({ profissionais, consultas });
+function Grafico({profissionais, consultas}: Props) {
+    let dados: Array<IDados> = useDadosGrafico({profissionais, consultas})
+
     return (
-        <SecaoEstilizada>
-            <ResponsiveContainer width="100%" height={350}>
-                <BarChart
-                    layout="vertical"
-                    data={dados}
-                    margin={{ top: 25, right: 40, left: 40, bottom: 20 }}
-                >
-                    <XAxis type="number"></XAxis>
-                    <YAxis type="category" dataKey="nome"></YAxis>
-                    <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
-                </BarChart>
-            </ResponsiveContainer>
-        </SecaoEstilizada>
+        <SecaoEtilizada>
+        <ResponsiveContainer width="100%" height={350}>
+            <BarChart
+                layout="vertical"
+                data={dados}
+                margin={{top: 25, right: 40, left: 40, bottom: 20}}
+            >
+                <XAxis type="number"></XAxis>
+                <YAxis type="category" dataKey="nome"></YAxis>
+                <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
+            </BarChart>
+        </ResponsiveContainer>
+        </SecaoEtilizada>
     )
 }
 
-export default Grafico;
+export default Grafico
